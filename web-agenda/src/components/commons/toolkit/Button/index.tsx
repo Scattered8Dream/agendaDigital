fa90320a9dfb58108bfa40ca0fb3999edba4bec8
EdsonNascimento <edsonnascimentoedson856@gmail.com>
2023-06-15 21:34:28 -0300

    feat (web-agenda components commons toolkit button): Implements Button component

---
+++ web-agenda/src/components/commons/toolkit/Button/index.tsx
@@ -0,0 +1,30 @@
+import React, { ButtonHTMLAttributes } from 'react'
+
+import { AnimatedLoadingIcon } from '../../../../assets/loadingIcon'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string
+  isLoading?: boolean
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  label,
+  isLoading,
+  ...rest
+}) => {
+  return (
+    <button
+      className="w-full bg-blue-700 text-neutral-100 font-medium text-lg hover:brightness-110 transition-all p-4"
+      disabled={isLoading}
+      {...rest}
+    >
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <AnimatedLoadingIcon />
+        </div>
+      ) : (
+        label
+      )}
+    </button>
+  )
+}
