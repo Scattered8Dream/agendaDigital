009c548041777f8bb1923cdcc6cf7fb7e3306cc3
EdsonNascimento <edsonnascimentoedson856@gmail.com>
2023-06-15 23:24:25 -0300

    fix (server-agenda): Delete server app

--- server-agenda/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import UsersRouter from "./users.routes";
-
-const RouterRouter = Router();
-
-RouterRouter.use("/users", UsersRouter);
-
-export default RouterRouter;
