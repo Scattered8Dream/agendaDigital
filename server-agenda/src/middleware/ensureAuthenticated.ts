a0abb79c4bd1bd2f4481013d336032fab8c3bae6
EdsonNascimento <edsonnascimentoedson856@gmail.com>
2023-06-16 23:59:15 -0300

    feat (server-agenda): Implements authentication verification middleware

---
+++ server-agenda/src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import authConfig from "../config/auth";
+import AppError from "../errors/AppError";
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+const ensureAuthenticated = (
+  request: Request,
+  _: Response,
+  next: NextFunction
+): void => {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("JWT token is missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      userId: sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError("Invalid JWT token", 401);
+  }
+};
+
+export default ensureAuthenticated;
