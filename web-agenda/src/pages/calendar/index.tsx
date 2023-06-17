dd0da9a91e261c7b829fe801bc31e1fae5176231
EdsonNascimento <edsonnascimentoedson856@gmail.com>
2023-06-16 22:46:34 -0300

    feat (web-agenda pages calendar): Add authenticated user validation

--- web-agenda/src/pages/calendar/index.tsx
+++ web-agenda/src/pages/calendar/index.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
 
 import { HeadComponent } from '../../components/commons/structure/HeadComponent'
 import { Schedule } from '../../components/pages/Calendar/Schedule'
+import { useAuth } from '../../hooks/auth'
 
 const CalendarPage: NextPage = () => {
+  const router = useRouter()
+
+  const { userData } = useAuth()
+
+  useEffect(() => {
+    if (!userData) {
+      toast.error(
+        'Acesso negado! Necessário autenticar pra acessar essa página'
+      )
+
+      router.push('/')
+    }
+  }, [userData])
+
   return (
     <>
       <HeadComponent title="Calendar Page" />
